7334999266ed907ccc0e62dc6016ddef809444e8
Aaron Tello-Wharton <tellowha@aston.ac.uk>
2015-11-12 03:29:44 +0000

    Creature Class Annotated

--- src/CreatureFactory.java
+++ src/CreatureFactory.java
@@ -10,12 +10,26 @@ public class CreatureFactory {
     public static Creature PLANKTON = new Plankton("Plankton");
 
     public static Creature[] CREATURES = {SHARK, SARDINE, PLANKTON};
+
     public static void init(){
 
     }
 
     public CreatureFactory(){
 
+    }
+
+    public Field populateField(){
+        int x = 100;
+        int y = 100;
+        Field field = new Field(x,y);
+        for(int a = 0; a < field.getWidth(); a++){
+            for(int b = 0; b < field.getDepth(); b++){
+                Location location = new Location(a,b);
+                field.place(getRandom(location),location);
+            }
+        }
+        return field;
     }
 
     public Creature getRandom(Location location) {
