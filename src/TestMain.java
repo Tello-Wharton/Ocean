f51089f7ec7b14e9fad35b50d25d9f1b4a54a425
A-R-T-W <tellowha@aston.ac.uk>
2015-12-01 12:02:58 +0000

    TestMain used for file writing atm

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Iterator;
@@ -8,5 +12,20 @@ import java.util.Iterator;
 public class TestMain {
     public static void main(String args[]){
 
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("data.csv", "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        writer.println("");
+        writer.println("The second line");
+
+        writer.close();
     }
+
+
 }
